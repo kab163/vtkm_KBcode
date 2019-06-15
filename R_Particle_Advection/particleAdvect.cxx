79de287db7c768333cd8ff4118fce13dc617c35c
Kristi Belcher <kab163@cs.uoregon.edu>
2019-06-14 17:28:37 -0700

    making updates to R_PA code, particularly command line parameters

--- R_Particle_Advection/particleAdvect.cxx
+++ R_Particle_Advection/particleAdvect.cxx
@@ -31,7 +31,7 @@
 #include "particles_array.h"
 
 #define TIMEINFO timeval
-
+#define THRESHOLD 1
 //----------------------------------------------------
 using namespace std;
 
@@ -40,8 +40,11 @@ using cpu = vtkm::cont::DeviceAdapterTagTBB;
 using gpu = vtkm::cont::DeviceAdapterTagCuda;
 
 void DeviceOracle(const int num) { 	
-  if(num < 1) vtkm::cont::GetRuntimeDeviceTracker().ForceDevice(gpu());//tracker.ForceDevice(gpu());
-  else vtkm::cont::GetRuntimeDeviceTracker().ForceDevice(cpu());
+  if(num > THRESHOLD) {
+    vtkm::cont::GetRuntimeDeviceTracker().ForceDevice(gpu());//tracker.ForceDevice(gpu());
+  } else {
+    vtkm::cont::GetRuntimeDeviceTracker().ForceDevice(cpu());
+  }
 }
 
 //----------------------------------------------------
@@ -57,14 +60,15 @@ DiffTime(const struct TIMEINFO &startTime,
 //----------------------------------------------------
 int main(int argc, char **argv)
 {
-  const int DimNum = 3;
-  const int OUT_NUM = 1024;
+  const int DimNum = 3; //3 demensional algorithm - value related to dim_xyz
+  //const int OUT_NUM = 1024;
 
-  if(argc != 5) std::cerr<< "usage: <exe> num_particles num_steps step_size dim_xyz";
-  const int NUMPART = std::atoi(argv[1]);
-  const int nSteps = std::atoi(argv[2]);
-  const double stepSize = std::atoi(argv[3]);
-  const int dim_xyz = std::atoi(argv[4]);
+  if(argc != 6) std::cerr<< "usage: <exe> fieldnum nP num_steps step_size dim_xyz";
+  const int fieldnum = std::atoi(argv[1]); //value used when creating the velocity field
+  const int nP = std::atoi(argv[2]); //number of particles
+  const int nSteps = std::atoi(argv[3]); //number of steps the particles will advect
+  const double stepSize = std::atoi(argv[4]); //how big each step is
+  const int dim_xyz = std::atoi(argv[5]); //dimensions (related to fieldnum)
   
   std::vector<vtkm::Vec<vtkm::Float32, DimNum> >vtkmVec2;
   vtkm::cont::ArrayHandle<vtkm::Vec<vtkm::Float32, DimNum> > field;
@@ -75,9 +79,9 @@ int main(int argc, char **argv)
   vtkm::Id3 dims(dim_xyz, dim_xyz, dim_xyz);
   vtkm::cont::DataSet ds;
   ds = dataSetBuilder.Create(dims, origin, spacing);
-  std::cerr<<"dataSetBuilder done \n";
+  std::cout<<"dataSetBuilder done \n";
 
-  #if 1
+  #if 0
   int numThreads = tbb::task_scheduler_init::automatic;
   numThreads = 12;
   tbb::task_scheduler_init init(numThreads);
@@ -85,7 +89,7 @@ int main(int argc, char **argv)
 
   //omp_set_num_threads(12);
 
-  for (int i = 0; i < NUMPART; i++)
+  for (int i = 0; i < fieldnum; i++)
   {   
     vtkm::Float32 x = 0;
     vtkm::Float32 y = 0;
@@ -107,7 +111,7 @@ int main(int argc, char **argv)
   std::vector<vtkm::Id> particleSteps;
   vtkm::Id pSteps;
 
-  for(int i=0; i <nSteps; i++)
+  for(int i=0; i <nP; i++)
   {
     vtkm::Float32 x = particles[i][0];
     vtkm::Float32 y = particles[i][1];
@@ -127,42 +131,18 @@ int main(int argc, char **argv)
   vtkm::worklet::ParticleAdvectionResult res;
   vtkm::worklet::ParticleAdvection particleAdvection;
  
-  //DeviceOracle(NUMPART);
+  DeviceOracle(nP);
   //vtkm::worklet::DispatcherMapField<vtkm::worklet::ParticleAdvection> paDispatcher(particleAdvection);
   //paDispatcher.SetDevice(gpu());
-
+  
   gettimeofday(&startTime2, 0);
   res = particleAdvection.Run(rk4, seedArray, particleStepsArray, nSteps);
   gettimeofday(&endTime2, 0);
-  
+
   double t2 = DiffTime(startTime2, endTime2);
-  cerr << "The time of Actual particle vtkm call is " << t2 << endl;
-/*
-  typedef typename vtkm::cont::ArrayHandle<vtkm::Vec<vtkm::Float32, DimNum> >::PortalConstControl PortalPosition;
-  PortalPosition readPortal = res.positions.GetPortalConstControl();
+  cout << "The time of Actual particle vtkm call is " << t2 << endl;
+  //cout << "Result of PA is " << res << endl;
 
-  typedef typename vtkm::cont::ArrayHandle<vtkm::Id>::PortalConstControl PortalStatus;
-  PortalStatus readPortal1 = res.status.GetPortalConstControl();
-
-  typedef typename vtkm::cont::ArrayHandle<vtkm::Id>::PortalConstControl PortalSteps;
-  PortalSteps readPortal2 = res.stepsTaken.GetPortalConstControl();
-
-  ofstream dbg;
-  char dbgFile[OUT_NUM];
-  sprintf(dbgFile, "output.txt");
-  dbg.open(dbgFile);
-  float outpt[DimNum];
-  int outnSteps;
-
-  for(int i=0; i < nSteps; i++)
-  {    
-    outnSteps = readPortal2.Get(i);
-    outpt[0] = readPortal.Get(i)[0];
-    outpt[1] = readPortal.Get(i)[1];
-    outpt[2] = readPortal.Get(i)[2];
-    dbg<<outpt[0]<<", "<<outpt[1]<<", "<<outpt[2]<<", "<<outnSteps<<"\n";
-  }	  
-*/
   return 0;   
 }	
 
